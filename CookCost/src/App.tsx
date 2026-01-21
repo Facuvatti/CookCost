2d1ed1750847081f5037e7c63496dc2c60a4cdeb
Facuvatti <facuvatti@gmail.com>
2026-01-21 10:42:39 -0300

    ahora configure correctamente al tailwindcss para que funcione con la clase dark y no el media. Se hace mediante un @custom-variant dark (&:where(.dark, .dark *)); en el archivo css donde se importó. De esa manera es en la v4

--- CookCost/src/App.tsx
+++ CookCost/src/App.tsx
@@ -1,26 +1,21 @@
 import './App.css'
 import Ingredients from './components/ingredients/ingredients'
-import { useEffect, useState } from 'react'
-const toggleDarkMode = (setIsDark: React.Dispatch<React.SetStateAction<boolean>>) => {
-  const root = document.documentElement;
-  const isDark = root.classList.toggle("dark");
-  localStorage.setItem("darkMode", String(isDark));
-  setIsDark(isDark);
-};
+import { useState, useEffect } from 'react'
 function App() {
-    const [isDark, setIsDark] = useState(false);
+    const [isDark, setIsDark] = useState<boolean>(()=>{
+        return localStorage.getItem("theme" )=== 'dark'
+    });
     useEffect(() => {
-        const html = document.documentElement;
-
-        const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-        const stored = localStorage.getItem("darkMode");
-
-        if (stored === "true" || (stored === null && prefersDark)) {
+        const html = document.documentElement
+        if (!html) return;
+        if (isDark) {
             html.classList.add("dark");
+            localStorage.setItem("theme", "dark");
         } else {
             html.classList.remove("dark");
+            localStorage.setItem("theme", "light");
         }
-    }, []);
+    }, [isDark]);
   return (
     <>
         <div className="max-w-4xl mx-auto mb-8 flex justify-between items-center">
@@ -28,10 +23,10 @@ function App() {
             <button 
                 id="darkModeToggle"
                 className="p-3 bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 rounded-full transition-colors duration-200 shadow-md"
-                onClick={()=>toggleDarkMode(setIsDark)}
+                onClick={()=>setIsDark(prev => !prev)}
             >
-                {isDark ? <svg id="sunIcon" className="w-6 h-6 text-yellow-400 hidden" fill="currentColor" viewBox="0 0 20 20"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"></path></svg>
-                        : <svg id="moonIcon" className="w-6 h-6 text-gray-800 hidden" fill="currentColor" viewBox="0 0 20 20"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
+                {isDark ? <svg id="sunIcon" className="w-6 h-6 text-yellow-400" fill="currentColor" viewBox="0 0 20 20"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"></path></svg>
+                        : <svg id="moonIcon" className="w-6 h-6 text-gray-800" fill="currentColor" viewBox="0 0 20 20"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
                 }
 
             </button>
